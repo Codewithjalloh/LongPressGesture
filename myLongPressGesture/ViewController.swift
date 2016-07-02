1eb564c285c3d74915c23e60e273e8908db12791
D.A Jalloh <info@codewithjalloh.com>
2016-07-02 23:47:15 +0100

    code complete

--- myLongPressGesture/ViewController.swift
+++ myLongPressGesture/ViewController.swift
@@ -9,15 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: "longPressGesture:")
+        longPressGesture.minimumPressDuration = 2.0
+        myImageView.addGestureRecognizer(longPressGesture)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func longPressGesture(gesture:UIGestureRecognizer) {
+        if(gesture.state == .Began) {
+            let alertController = UIAlertController(title: "Know thyself", message: "Master Sun said", preferredStyle: .Alert)
+            let okAction = UIAlertAction(title: "OK", style: .Default, handler: {(action) in })
+            alertController.addAction(okAction)
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+        
     }
 
 
